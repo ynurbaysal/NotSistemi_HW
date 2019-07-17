8da43a7a10097d2168f63f69c71b6447f2fefbf5
nurbaysal <nurbysl__@hotmail.com>
2019-07-17 18:51:26 +0300

    project

---
+++ NotSistemi_HW/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace NotSistemi_HW
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNot = new System.Windows.Forms.Button();
+            this.txtNot = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnNot
+            // 
+            this.btnNot.Location = new System.Drawing.Point(36, 52);
+            this.btnNot.Name = "btnNot";
+            this.btnNot.Size = new System.Drawing.Size(75, 23);
+            this.btnNot.TabIndex = 0;
+            this.btnNot.Text = "Notunuz";
+            this.btnNot.UseVisualStyleBackColor = true;
+            this.btnNot.Click += new System.EventHandler(this.BtnNot_Click);
+            // 
+            // txtNot
+            // 
+            this.txtNot.Location = new System.Drawing.Point(25, 12);
+            this.txtNot.Name = "txtNot";
+            this.txtNot.Size = new System.Drawing.Size(100, 20);
+            this.txtNot.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtNot);
+            this.Controls.Add(this.btnNot);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNot;
+        private System.Windows.Forms.TextBox txtNot;
+    }
+}
+
---
+++ NotSistemi_HW/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NotSistemi_HW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Girilen not 0 - 30 araligi ise "FF" 30 - 50 araligi "DD" 50 - 70
+            //"BB" 70 - 100 "AA" aldınız seklinde kullaniciyi uyariniz..
+
+            
+
+
+
+
+        }
+
+        private void BtnNot_Click(object sender, EventArgs e)
+        {
+            int not = int.Parse(txtNot.Text);
+            txtNot.Focus();
+            txtNot.Clear();
+
+            if (not<30 && not>0)
+            {
+                MessageBox.Show("FF Aldınız");
+            }
+            if (not<50 && not>= 30)
+            {
+                MessageBox.Show("DD Aldınız");
+            }
+            if (not<70 && not>=50)
+            {
+                MessageBox.Show("BB Aldınız");
+            }
+            if (not<100 && not>=70)
+            {
+                MessageBox.Show("AA ALdınız");
+            }
+            
+
+        }
+
+
+    
+
+
+    }
+}
+
